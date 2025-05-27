f563408bd2eb32fc5b3cbc8635f0b6043d86b9e2
praveen135333 <159537399+praveen135333@users.noreply.github.com>
2025-05-27 13:27:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var ball = document.querySelector("#ball")
+var ball = document.querySelector("#ball");
 
-gsap.to("#ball",{
+gsap.to(ball,{
     y:640,
     scrollTrigger:{
         trigger:"body",
         pin:true,
         scrub:3,
     }
-})
+});
