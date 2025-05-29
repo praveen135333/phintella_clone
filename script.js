eb958a39fb9e096e7c76a6cb4b84347ed2168297
praveen135333 <praveensingh135333@gmail.com>
2025-05-29 13:47:26 +0530

    checking

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-var ball = document.querySelector("#ball")
+var ball = document.querySelector("#ball");
 
-gsap.to("#ball",{
+gsap.to(ball,{
     y:640,
     scrollTrigger:{
         trigger:"body",
         pin:true,
-        scrub:3,
+        scrub:2
+    },
+    onComplete:()=>{
+        gsap.to(ball,{
+            x:1400,
+            scrollTrigger:{
+                trigger:"body",
+                pin:true,
+                scrub:2
+            }
+        })
     }
-})
+});
